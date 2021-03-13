0e5d32ec0556d141791572b938b486ae29e94862
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-13 13:49:07 +0100

    Set default DataContext

--- Animator/App.axaml.cs
+++ Animator/App.axaml.cs
@@ -1,3 +1,7 @@
+using System.Collections.ObjectModel;
+using Animator.ViewModels;
+using Animator.ViewModels.Animation;
+using Animator.ViewModels.Style;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -15,10 +19,60 @@ namespace Animator
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+                var project = CreateProject();
+
+                desktop.MainWindow = new MainWindow()
+                {
+                    DataContext = project
+                };
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private static ProjectViewModel CreateProject()
+        {
+            return new ProjectViewModel()
+            {
+                Name = "Project1",
+                Path = "",
+                Styles = new ObservableCollection<StyleViewModel>()
+                {
+                    new StyleViewModel()
+                    {
+                        Name = "style1",
+                        Selector = "",
+                        Setters = new ObservableCollection<SetterViewModel>()
+                        {
+                            new SetterViewModel() { Property = "", Value = "" }
+                        },
+                        Animations = new ObservableCollection<AnimationViewModel>()
+                        {
+                            new AnimationViewModel()
+                            {
+                                Name = "animation1",
+                                KeyFrames = new ObservableCollection<KeyFrameViewModel>()
+                                {
+                                    new KeyFrameViewModel()
+                                    {
+                                        Setters = new ObservableCollection<SetterViewModel>()
+                                        {
+                                            new SetterViewModel() { Property = "", Value = "" }
+                                        }
+                                    },
+                                    new KeyFrameViewModel()
+                                    {
+                                        Setters = new ObservableCollection<SetterViewModel>()
+                                        {
+                                            new SetterViewModel() { Property = "", Value = "" }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
         }
     }
 }
