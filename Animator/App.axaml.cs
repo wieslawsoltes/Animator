2ce0a58c24e53bbf3ff60127fb949700269c04df
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-17 21:43:13 +0200

    Move

--- Animator/App.axaml.cs
+++ Animator/App.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.ObjectModel;
 using Animator.ViewModels;
+using Animator.ViewModels.Animation;
+using Animator.ViewModels.Style;
 using Animator.Views;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
@@ -13,9 +17,60 @@ public class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
+    private static ProjectViewModel CreateDemo()
+    {
+        return new ProjectViewModel
+        {
+            Name = "Project1",
+            Path = "",
+            Styles = new ObservableCollection<StyleViewModel>
+            {
+                new StyleViewModel
+                {
+                    Name = "style1",
+                    Selector = "Rectangle.animation",
+                    Setters = new ObservableCollection<SetterViewModel>
+                    {
+                        new SetterViewModel { Property = "Opacity", Value = "1.0" }
+                    },
+                    Animations = new ObservableCollection<AnimationViewModel>
+                    {
+                        new AnimationViewModel
+                        {
+                            Name = "animation1",
+                            Duration = TimeSpan.FromSeconds(2),
+                            Delay = TimeSpan.FromSeconds(0),
+                            KeyFrames = new ObservableCollection<KeyFrameViewModel>
+                            {
+                                new KeyFrameViewModel
+                                {
+                                    KeyTime = TimeSpan.FromSeconds(0),
+                                    Setters = new ObservableCollection<SetterViewModel>
+                                    {
+                                        new SetterViewModel { Property = "Opacity", Value = "1.0" },
+                                        new SetterViewModel { Property = "RotateTransform.Angle", Value = "0" },
+                                    }
+                                },
+                                new KeyFrameViewModel
+                                {
+                                    KeyTime = TimeSpan.FromSeconds(2),
+                                    Setters = new ObservableCollection<SetterViewModel>
+                                    {
+                                        new SetterViewModel { Property = "Opacity", Value = "0.0" },
+                                        new SetterViewModel { Property = "RotateTransform.Angle", Value = "360" },
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+
     public override void OnFrameworkInitializationCompleted()
     {
-        var project = ProjectViewModel.Create();
+        var project = CreateDemo();
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
--- Animator/ViewModels/ProjectViewModel.cs
+++ Animator/ViewModels/ProjectViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using Animator.ViewModels.Animation;
+﻿using System.Collections.ObjectModel;
 using Animator.ViewModels.Style;
 using ReactiveUI;
 
@@ -28,56 +26,5 @@ public class ProjectViewModel : ViewModelBase
     {
         get => _styles;
         set => this.RaiseAndSetIfChanged(ref _styles, value);
-    }
-
-    public static ProjectViewModel Create()
-    {
-        return new ProjectViewModel
-        {
-            Name = "Project1",
-            Path = "",
-            Styles = new ObservableCollection<StyleViewModel>
-            {
-                new StyleViewModel
-                {
-                    Name = "style1",
-                    Selector = "Rectangle.animation",
-                    Setters = new ObservableCollection<SetterViewModel>
-                    {
-                        new SetterViewModel { Property = "Opacity", Value = "1.0" }
-                    },
-                    Animations = new ObservableCollection<AnimationViewModel>
-                    {
-                        new AnimationViewModel
-                        {
-                            Name = "animation1",
-                            Duration = TimeSpan.FromSeconds(2),
-                            Delay = TimeSpan.FromSeconds(0),
-                            KeyFrames = new ObservableCollection<KeyFrameViewModel>
-                            {
-                                new KeyFrameViewModel
-                                {
-                                    KeyTime = TimeSpan.FromSeconds(0),
-                                    Setters = new ObservableCollection<SetterViewModel>
-                                    {
-                                        new SetterViewModel { Property = "Opacity", Value = "1.0" },
-                                        new SetterViewModel { Property = "RotateTransform.Angle", Value = "0" },
-                                    }
-                                },
-                                new KeyFrameViewModel
-                                {
-                                    KeyTime = TimeSpan.FromSeconds(2),
-                                    Setters = new ObservableCollection<SetterViewModel>
-                                    {
-                                        new SetterViewModel { Property = "Opacity", Value = "0.0" },
-                                        new SetterViewModel { Property = "RotateTransform.Angle", Value = "360" },
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        };
     }
 }
